e94be98b96f310f20cb3433d6d4bb2b483d0acd7
Akash <akash.gopinath008@gmail.com>
2024-12-29 18:33:28 +0000

    More neovim config

--- user/app/nvim/lua/akash/lazy.lua
+++ user/app/nvim/lua/akash/lazy.lua
@@ -11,4 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("akash.plugins")
+
+require("lazy").setup("akash.plugins", {
+
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+
+  change_detection = {
+    notify = false,
+  },
+})
