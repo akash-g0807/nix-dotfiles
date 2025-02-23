70697fc84d389a142a8e8b00214120e72a13eb2d
Akash <akash.gopinath008@gmail.com>
2025-02-23 21:04:31 +0000

    Updated vscode config

--- user/app/vscode/vscode.nix
+++ user/app/vscode/vscode.nix
@@ -5,7 +5,7 @@
 programs.vscode = {
   enable = true;
   package = pkgs.vscode.fhs;
-  extensions = with pkgs.vscode-extensions; [
+  profiles.default.extensions = with pkgs.vscode-extensions; [
     dracula-theme.theme-dracula
     vscodevim.vim
     yzhang.markdown-all-in-one
